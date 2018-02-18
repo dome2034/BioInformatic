4ff0ab99618c23a431be7c97474503e9097506eb
Thanakit <dome2034@gmail.com>
2018-02-18 12:54:45 +0700

    5

--- Project/app.py
+++ Project/app.py
@@ -12,7 +12,6 @@ def score(s,l,DNA):
         DNACutCal=(DNA[i][(s[row]-1):((s[row]-1)+l)])
         i+=1
         j=0
-        print(DNACutCal)
         for DNAChar in DNACutCal:            
             if (DNAChar == 'A'):
                 A[j]+=1
@@ -37,11 +36,6 @@ def score(s,l,DNA):
             Max = C[i]
         ConsensusVal[i] = Max
     ResultScore = sum(ConsensusVal)
-    print("A : ",A)
-    print("T : ",T)
-    print("G : ",G)
-    print("C : ",C)
-    print("ConsensusVal : ",ConsensusVal)
     return ResultScore
 
 def partialScore(s,l,t,DNA):
@@ -55,8 +49,7 @@ def partialScore(s,l,t,DNA):
         DNACut.append(DNA[i][(s[row]-1):((s[row]-1)+l)])
         DNACutCal=(DNA[i][(s[row]-1):((s[row]-1)+l)])
         i+=1
-        j=0
-        print(DNACutCal)     
+        j=0    
         for DNAChar in DNACutCal:            
             if (DNAChar == 'A'):
                 A[j]+=1
@@ -81,21 +74,11 @@ def partialScore(s,l,t,DNA):
         if(C[i] > Max):
             Max = C[i]
         ConsensusVal[i] = Max
-    ResultScore = sum(ConsensusVal)
-    print("t : ",t)
-    print("i : ",len(s))
-    print("l : ",l)
-    OptScore = (t-len(s))*l
-    print("A : ",A)
-    print("T : ",T)
-    print("G : ",G)
-    print("C : ",C)
-    print("ConsensusVal : ",ConsensusVal)
-    print("Consensus Score : ",ResultScore)
-    fullScore = ResultScore+OptScore
-    return fullScore
-
+    ResultScore = sum(ConsensusVal)+(t-len(s))*l
+    print("Partial Score : ",sum(ConsensusVal))
+    return ResultScore
 #============================= 
+    
 s0=[1,1,11,11]
 s1=[1]
 l=8
@@ -106,5 +89,5 @@ for row in fo:
     DNA.append(row.replace('\n',''))
 fo.close()
 
-print("Consensus Score : ",score(s0,l,DNA))
+print("Full Score : ",score(s0,l,DNA))
 print("Optimistic Score : ",partialScore(s1,l,t,DNA))
