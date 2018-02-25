f0549d268c8ce01eec3d078c28f4266cd916385c
Dome2034 <dome2034@gmail.com>
2018-02-25 11:02:12 +0700

    25-02-2018_1102

--- Project/a1-motif-600632014.py
+++ Project/a1-motif-600632014.py
@@ -1,3 +1,4 @@
+import sys,os
 
 # Function definition is here
 def score(s,DNA,l):
@@ -77,17 +78,6 @@ def partialScore(s,i,DNA,l):
                     MaxChar[j] = 'C'
     ResultScore = sum(MaxCharVal)
     return ResultScore,MaxChar
-
-def nextLeaf(a,L,k):
-    a=[0]+a
-    for i in range(L,0,-1):
-        if a[i]<k :
-            a[i]=a[i]+1
-            a.pop(0)
-            return a
-        a[i]=1
-    a.pop(0)
-    return a
     
 def nextVertex(a,i,L,k):
     if i<L:
@@ -108,6 +98,7 @@ def byPass(a,i,L,k):
     return (a,0)
 
 def branchAndBoundMotifSearch(DNA,t,n,l):
+    progress = "."
     countLoop = 0
     s = [0]*t
     scoreNow = 0
@@ -128,27 +119,79 @@ def branchAndBoundMotifSearch(DNA,t,n,l):
                 BestMotif = s.copy()
             s,i = nextVertex(s,i,t,(n - l + 1))
         countLoop += 1
-        #if (countLoop%10000 == 0):
-            #print(countLoop+1,"score :",scoreNow,"best score :",BestScore,s,"best :",BestMotif)
+        #-- print progress
+        if (countLoop%100000 == 0):
+            os.system('cls')
+            print("==================")
+            print("Loading ",progress)
+            print("==================")
+            if (len(progress) == 10):
+                progress = "."
+            else:
+                progress += "."
+                
     Consensus = score(BestMotif,DNA,l)[1]
     ResultBestMotif = [x+1 for x in BestMotif]
-    #print("=== run finish ===")
+    return countLoop,ResultBestMotif,BestScore,Consensus
+
+def greedyMotifSearch(DNA,t,n,l):
+    countLoop = 0
+    BestMotif = [0]*t
+    s = [0]*t
+    for s[0] in range(0,(n-l+1)):
+        for s[1] in range(0,(n-l+1)):
+            if (partialScore(s,2,DNA,l)[0] > partialScore(BestMotif,2,DNA,l)[0]):
+                BestMotif[0] = s[0]
+                BestMotif[1] = s[1]
+            countLoop += 1
+    s[0] = BestMotif[0]
+    s[1] = BestMotif[1]
+    for i in range(2,t):
+        for s[i] in range(0,(n-l+1)):
+            if (partialScore(s,i+1,DNA,l)[0] > partialScore(BestMotif,i+1,DNA,l)[0]):
+                BestMotif[i] = s[i]
+            countLoop += 1
+        s[i] = BestMotif[i]
+    #print(countLoop)
+    BestScore = score(BestMotif,DNA,l)[0]
+    Consensus = score(BestMotif,DNA,l)[1]
+    ResultBestMotif = [x+1 for x in BestMotif]   
+    
     return countLoop,ResultBestMotif,BestScore,Consensus
 
 #============================= 
     
-n = 60
-l = 10
-t = 5
-fo = open('test2.fasta')
-DNA=[]
-for row in fo:
-    DNA.append(row.replace('\n',''))
-fo.close()
+def main(argv):
+    if (len(sys.argv) == 4):
+        FileName = sys.argv[1]
+        if (os.path.exists('./' + FileName)):
+            fo = open(str(FileName))
+            DNA=[]
+            for row in fo:
+                if (row[0] != '>'):
+                    DNA.append(row.replace('\n',''))
+            fo.close()
+            TypeMotif = sys.argv[2]
+            l = int(str(sys.argv[3]))
+            n = len(DNA[0])
+            t = len(DNA)
+            if (TypeMotif.upper() == "B"):
+                countLoop,ResultBestMotif,BestScore,Consensus = branchAndBoundMotifSearch(DNA,t,n,l)
+            elif (TypeMotif.upper() == "G"):
+                countLoop,ResultBestMotif,BestScore,Consensus = greedyMotifSearch(DNA,t,n,l)
+            else:
+                print("error: Motif finding type must be B or G only.\n")
+                exit()
+            os.system('cls')
+            print("Loop :",countLoop)
+            print("BestMotif :",ResultBestMotif)
+            print("BestScore :",BestScore)
+            print("Consensus",Consensus)    
+        else:
+            print("error: file",FileName,"not found!\n")
+            exit()
+    else:
+        print("usage: python a1-motif-600632014.py [fasta inputfile] [type of motif finding] [length of sequence].\nPlease, Try again.\n")
 
-countLoop,ResultBestMotif,BestScore,Consensus = branchAndBoundMotifSearch(DNA,t,n,l)
-
-print("Loop :",countLoop)
-print("BestMotif :",ResultBestMotif)
-print("BestScore :",BestScore)
-print("Consensus",Consensus)
+if __name__ == "__main__":
+	main(sys.argv)
